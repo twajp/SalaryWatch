27773a4ed5dc22fb0be7073e361c498a55a4c1ec
TWA <64235761+twajp@users.noreply.github.com>
2024-04-15 15:18:34 +0900

    たたき台完成

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,153 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    // 縦向き
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SalaryWatch',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorSchemeSeed: Colors.green,
         useMaterial3: true,
+        brightness: Brightness.light,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.green,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const StopwatchPage(title: 'SalaryWatch'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class StopwatchPage extends StatefulWidget {
+  const StopwatchPage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  StopwatchPageState createState() => StopwatchPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class StopwatchPageState extends State<StopwatchPage> {
+  Timer? _stopwatchTimer;
+  Timer? _clockTimer;
+  Duration _elapsed = Duration.zero;
+  DateTime _currentTime = DateTime.now();
+  bool _isRunning = false;
+  int hourlyWage = 1200;
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    // 時計用のタイマーを設定して1秒ごとに更新
+    _clockTimer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        _currentTime = DateTime.now();
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // タイマーのキャンセル
+    _stopwatchTimer?.cancel();
+    _clockTimer?.cancel();
+    super.dispose();
+  }
+
+  void _toggleStopwatch() {
+    if (_isRunning) {
+      // ストップウォッチが動作中の場合、停止
+      _stopwatchTimer?.cancel();
+    } else {
+      // ストップウォッチが停止している場合、開始
+      _stopwatchTimer =
+          Timer.periodic(const Duration(milliseconds: 10), (timer) {
+        setState(() {
+          _elapsed += const Duration(milliseconds: 10);
+        });
+      });
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _isRunning = !_isRunning; // ストップウォッチの状態をトグル
+    });
+  }
+
+  void _resetStopwatch() {
+    // ストップウォッチをリセット
+    _stopwatchTimer?.cancel();
+    setState(() {
+      _elapsed = Duration.zero;
+      _isRunning = false;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    // 端末のロケール情報を取得
+    final Locale currentLocale = Localizations.localeOf(context);
+    // 通貨マークを取得
+    String currencySymbol =
+        NumberFormat.simpleCurrency(locale: currentLocale.toString())
+            .currencySymbol;
+    NumberFormat format = NumberFormat("#,##0.00");
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        centerTitle: true,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            '${_elapsed.inHours.toString().padLeft(2, '0')}:'
+            '${(_elapsed.inMinutes % 60).toString().padLeft(2, '0')}:'
+            '${(_elapsed.inSeconds % 60).toString().padLeft(2, '0')}.'
+            '${(_elapsed.inMilliseconds % 1000 / 10).toStringAsFixed(0).padLeft(2, '0')}',
+            style: const TextStyle(fontSize: 36),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            '$currencySymbol'
+            '${format.format((_elapsed.inMilliseconds * (hourlyWage / 60 / 60 / 1000)))}',
+            style: const TextStyle(fontSize: 36),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _toggleStopwatch,
+                child: Text(_isRunning == false ? '開始' : '停止'),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: _resetStopwatch,
+                child: const Text('リセット'),
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
